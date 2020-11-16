6d4d4ba5113aa2199c6426dc576b1b02757bb848
Albert Sun <33230608+albertyusun@users.noreply.github.com>
2020-11-15 23:04:01 -0500

    add jupyter notebook

--- clean.py
+++ clean.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+# Read Datafile
+pd.set_option('display.max_columns', None)
 a = pd.read_csv("rawJailDataIntern.csv")
-
 print(a.head())
+
+# Compare admissions and discharge on year level; drop weekly/daily
+
